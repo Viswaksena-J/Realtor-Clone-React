c7ef038a2308971845ff3c8a9c2d2ccc9c03ec3f
Viswaksena Jayam <97895225+Viswaksena-J@users.noreply.github.com>
2023-10-05 15:04:08 +0530

    The 'Continue with Google' feature has been successfully connected with OAuth functionality

--- src/components/OAuth.jsx
+++ src/components/OAuth.jsx
@@ -1,8 +1,47 @@
-import React from 'react'
-import {FcGoogle} from 'react-icons/fc'
+import { getAuth } from "firebase/auth";
+import React from "react";
+import { FcGoogle } from "react-icons/fc";
+import { toast } from "react-toastify";
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { getDoc } from "firebase/firestore";
+import { db } from "../firebase";
+import { doc, serverTimestamp, setDoc } from "firebase/firestore";
+import { useNavigate } from "react-router";
 
 export default function OAuth() {
+  const navigate = useNavigate();
+  async function onGoogleClick() {
+    try {
+      const auth = getAuth();
+      const provider = new GoogleAuthProvider();
+      const result = await signInWithPopup(auth, provider);
+      const user = result.user;
+
+      //Check user is new or not
+      const docRef = doc(db, "users", user.uid);
+      const docSnap = await getDoc(docRef);
+
+      if (!docSnap.exists()) {
+        await setDoc(docRef, {
+          name: user.displayName,
+          email: user.email,
+          timestamp: serverTimestamp(),
+        });
+      }
+      navigate("/");
+    } catch (error) {
+      toast.error("Coudn't authorize with Google");
+      console.log(error);
+    }
+  }
   return (
-    <button className='flex items-center justify-center w-full bg-red-700 text-white px-7 py-3 uppercase text-sm font-medium hover:bg-red-800 active:bg-red-900 shadow-md hover:shadow-lg active:shadow-lg transition duration-150 ease-in-out rounded'><FcGoogle className='text-2xl rounded-full bg-white mr-2'/>Continue with Google</button>
-  )
+    <button
+      type="button"
+      onClick={onGoogleClick}
+      className="flex items-center justify-center w-full bg-red-700 text-white px-7 py-3 uppercase text-sm font-medium hover:bg-red-800 active:bg-red-900 shadow-md hover:shadow-lg active:shadow-lg transition duration-150 ease-in-out rounded"
+    >
+      <FcGoogle className="text-2xl rounded-full bg-white mr-2" />
+      Continue with Google
+    </button>
+  );
 }
