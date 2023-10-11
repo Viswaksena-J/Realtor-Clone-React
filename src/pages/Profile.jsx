d2bc61ca67edfb92e2e65e1e3484dcdfba3439d5
Viswaksena Jayam <97895225+Viswaksena-J@users.noreply.github.com>
2023-10-11 13:52:20 +0530

    add button for 'sell or rent property'

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -5,6 +5,8 @@ import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import { doc, updateDoc } from "firebase/firestore";
 import { db } from "../firebase";
+import { FcHome } from "react-icons/fc";
+import { Link } from "react-router-dom";
 
 export default function Profile() {
   const auth = getAuth();
@@ -27,10 +29,10 @@ export default function Profile() {
   };
   async function onSubmit() {
     try {
-      if(auth.currentUser.displayName !== name){
+      if (auth.currentUser.displayName !== name) {
         await updateProfile(auth.currentUser, {
-        displayName: name,
-        })
+          displayName: name,
+        });
 
         const docRef = doc(db, "users", auth.currentUser.uid);
         await updateDoc(docRef, {
@@ -88,6 +90,12 @@ export default function Profile() {
               </p>
             </div>
           </form>
+          <button type="submit" className="w-full bg-blue-600 text-white uppercase py-3 px-7 text-sm font-medium rounded shadow-md hover:bg-blue-700 transition duration-150 ease-in-out hover:shadow-lg active:bg-blue-800">
+            <Link to="/create-listing" className="flex justify-center items-center">
+              <FcHome className="mr-2 text-3xl bg-red-200 rounded-full p-1 border-2"/>
+              Sell or Rent your property
+            </Link>
+          </button>
         </div>
       </section>
     </>
